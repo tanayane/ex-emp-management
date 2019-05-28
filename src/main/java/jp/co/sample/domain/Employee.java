6c44536d17a87e99464c982cddb4a99d558cb42f
ayane.tanaka <ayane.tanaka@rakus.co.jp>
2019-05-28 10:15:10 +0900

    従業員クラスの作成

---
+++ src/main/java/jp/co/sample/domain/Employee.java
@@ -0,0 +1,18 @@
+package jp.co.sample.domain;
+
+import java.time.LocalDate;
+
+public class Employee {
+	private Integer id;
+	private String name;
+	private String image;
+	private String gender;
+	private LocalDate hireDate;
+	private String mailAddress;
+	private String zipCode;
+	private String address;
+	private String telephone;
+	private Integer salary;
+	private String characteristics;
+	private Integer dependentsCount;
+}
