f8ef3d4f0089db1bf0feca848f0f6f4e0c1448da
ayane.tanaka <ayane.tanaka@rakus.co.jp>
2019-05-28 12:25:43 +0900

    トランザクション処理指定追加

--- src/main/java/jp/co/sample/service/EmployeeService.java
+++ src/main/java/jp/co/sample/service/EmployeeService.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import jp.co.sample.domain.Employee;
 import jp.co.sample.repository.EmployeeRepository;
 
 @Service
+@Transactional
 public class EmployeeService {
 
 	@Autowired
