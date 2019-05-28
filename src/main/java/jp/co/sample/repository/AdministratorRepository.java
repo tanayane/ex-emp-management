7fbbfd9374b6c7334df262614e0e7099e3f004b4
ayane.tanaka <ayane.tanaka@rakus.co.jp>
2019-05-28 11:28:35 +0900

    不必要なimport 削除

--- src/main/java/jp/co/sample/repository/AdministratorRepository.java
+++ src/main/java/jp/co/sample/repository/AdministratorRepository.java
@@ -1,6 +1,5 @@
 package jp.co.sample.repository;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -1,6 +1,5 @@
 package jp.co.sample.service;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
